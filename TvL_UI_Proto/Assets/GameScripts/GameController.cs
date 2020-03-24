0a58c7d554189a1bde0411d516c46bdb5b777bba
Miles Smith <milessmith1809@gmail.com>
2020-03-23 17:27:47 -0700

    Code updates to ui and game controller
    building grid system and board tracker for keeping track of board states

--- TvL_UI_Proto/Assets/GameScripts/GameController.cs
+++ TvL_UI_Proto/Assets/GameScripts/GameController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public string[] players = new string[2]; //holds the players 
+    public List<int[]> board = new List<int[]>();
+    public  int[] boardSpace = new int[3];
+
+    public int currentTurn; // tracks player order and number of turns 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTurn = 0;
+        for (int x = 0; x < 5; x++)
+        {
+            boardSpace[0] = x;
+            for(int y = 0; y < 5; y++)
+            {
+                boardSpace[1] = y;
+                Debug.Log("x = " + x + ", y = " +y);
+                if ((x == 0 && y == 0) || (x == 4 && y == 0) || (x == 0 && y == 4) || (x == 4 && y == 4))
+                {
+                    boardSpace[2] = 1;
+                }
+                else
+                {
+                    boardSpace[2] = 0;
+                }
+                board.Add(boardSpace);
+            }
+        }
+        foreach(int[] space in board)
+        {
+            Debug.Log("X = " + space[0]);
+            Debug.Log("Y = " + space[1]);
+            Debug.Log("******************");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
