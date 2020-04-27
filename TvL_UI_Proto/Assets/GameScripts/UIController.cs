03befc7d5f61a921cbfb7cc1577043a980ac1f67
Miles Smith <milessmith1809@gmail.com>
2020-04-27 16:27:20 -0700

    Code updates on UIController
    Still debugging issues related to accessing a list of unity game objects

--- TvL_UI_Proto/Assets/GameScripts/UIController.cs
+++ TvL_UI_Proto/Assets/GameScripts/UIController.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
-    public GameObject[,] board = new GameObject[4,4];
-    public GameObject[] outOfPlay = new GameObject[20];
+    public List<Button> buttons;
+    public Dictionary<Button,string> grid;
 
-    public void BoardSetup()
+    public void BoardSetup(List<Button> buttonElments)
     {
-        GameObject tigerOne = (GameObject)Instantiate(Resources.Load("TigerPiece")); 
-        tigerOne. 
-
+        for(int i =0; i < 25; i++)
+        {
+            Debug.Log("Button name is " + buttonElments[i].name);
+            // add a new button + string to the dict 
+            grid.Add(buttonElments[i], "");
+            // check if the button is a corner space 
+            if (buttonElments[i].name == "GridSpace" || buttonElments[i].name == "GridSpace4" || buttonElments[i].name == "GridSpace20" || buttonElments[i].name == "GridSpace24")
+            {
+                //update the value in the dict 
+                grid[buttonElments[i]] = "T";
+                //set the actual text in game to the correct char
+                buttonElments[i].GetComponentInChildren<Text>().text = "T";
+            }
+        }
     }
 
-    void Start()
+    void Awake()
     {
-
+        Debug.Log("list is size is " + buttons.Count);
+        BoardSetup(buttons);  
     }
 }
