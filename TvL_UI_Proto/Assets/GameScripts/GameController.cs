faf83cc079c2d93e68df52e1a3753fa3984e9668
Miles Smith <milessmith1809@gmail.com>
2020-04-28 15:52:19 -0700

    finally fixed the null ref errors in UIController
    Unity had two instances of a script derived from monoBehaviour running at the same time cuasing the ref errors ar run time. Fixed by deleting all scripts from all game objects and reconnecting them one by one

---
+++ TvL_UI_Proto/Assets/GameScripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> buttons;
+    public Dictionary<int[], string> grid;
+    public void BoardSetup(List<Button> buttonElments)
+    {
+        int count = 0;
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Debug.Log("Button name is " + buttonElments[count].name);
+                // check if the button is a corner space 
+                if (buttonElments[count].name == "GridSpace" || buttonElments[count].name == "GridSpace4" || buttonElments[count].name == "GridSpace20" || buttonElments[count].name == "GridSpace24")
+                {
+                    //update the value in the dict 
+                    buttonElments[count].GetComponentInChildren<Text>().text = "T";
+                }
+                int[] cord = {x,y};
+                string bName = buttonElments[count].name;
+
+                Debug.Log(bName);
+
+               // grid.Add(cord, bName);
+                count += 1;
+                Debug.Log("x: " + x + " y: " + y);
+            }     
+        }
+    }
+    void Awake()
+    {
+        Debug.Log("list is size is " + buttons.Count);
+        BoardSetup(buttons);
+    }
+}
